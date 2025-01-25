bf3067f26766b321dad3e4b027cb167f40c633c0
chlopezgis <chlopezgis@gmail.com>
2025-01-25 12:22:49 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/* Crear mapa */
+var map = L.map('map').setView([-11.928499721110041, -77.05091118363637], 13);
+
+/* Agregar TileLayer */
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19, 
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+/* Agregar WMS */
+wmsComas = L.tileLayer.wms("http://localhost:8080/geoserver/Comas/wms?", {
+            layers:'manzanas',
+            format: 'image/png',
+            transparent: true,
+            attribution: "chlopezgis"
+            }).addTo(map)
