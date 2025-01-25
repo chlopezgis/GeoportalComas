9d94161f51abb1e36a03bbe361f19e9cd11795ba
chlopezgis <chlopezgis@gmail.com>
2025-01-25 12:45:05 -0500

    update

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ var map = L.map('map').setView([-11.928499721110041, -77.05091118363637], 13);
 /* Agregar TileLayer */
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    minZoom: 1,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 /* Agregar WMS */
 wmsComas = L.tileLayer.wms("http://localhost:8080/geoserver/Comas/wms?", {
             layers:'manzanas',
+            maxZoom: 19,
             format: 'image/png',
             transparent: true,
             attribution: "chlopezgis"
